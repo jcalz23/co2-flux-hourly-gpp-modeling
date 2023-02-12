2ccda99f4928b812f18539e6a9572fa8e5490456
m-chau <mchau.w@live.com>
2023-02-12 02:11:29 +0000

    move common methods to data-pipeline-lib

---
+++ code/src/tools/data_pipeline_lib.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+from IPython.display import display
+
+def get_min_max(df):
+  return (df.min(), df.max())
+
+def get_min_max_datetime(df):
+  return (pd.to_datetime(df).min(), pd.to_datetime(df).max())
+
+def is_leap_year(year):
+  return year%4 == 0 ;
+
+def data_cleanup(data_dir, site_id_file_df, target, target_qc, features):
+  data_df = None
+  # qc_flag_dtype = CategoricalDtype([0, 1, 2, 3], ordered=True)
+  qc_flags_features = [s for s in features if "_QC" in s]
+
+  # Iterate through each site:
+  for i, r in site_id_file_df.iterrows():        
+    if not r.filename or type(r.filename) != type(""):
+      print(f'\nERROR: {r.site_id} is mssing hourly data.')
+      continue
+
+    # Get only `features` from file
+    local_filename = data_dir + os.sep + r.filename
+    site_df = pd.read_csv(local_filename, usecols = features +  target + target_qc)
+    site_df['datetime'] = pd.to_datetime(site_df['datetime'])
+    site_df['date'] = pd.to_datetime(site_df['date'])
+    site_df['minute'] = site_df['datetime'].dt.minute
+    if len(qc_flags_features) != 0:
+      site_df[qc_flags_features] = site_df[qc_flags_features].astype('int')
+    site_df['site_id'] = r.site_id
+
+    # Remove zero or negative SW
+    site_df.drop(site_df[site_df['SW_IN_ERA'] <= 0].index, inplace = True)
+
+    # Drop rows with NAs for Target Variable
+    site_df.dropna(subset=target, axis=0, inplace=True)
+
+    # Drop rows with bad NEE_VUT_REF_QC (aka bad GPP records)
+    site_df.drop(site_df[site_df[target_qc[0]] == 3].index, inplace = True)
+    site_df.drop(target_qc, axis=1, inplace=True)
+
+    # Drop rows with any NA
+    site_df.dropna(axis=0, inplace=True)
+
+    print(f"{r.site_id}: {site_df.shape}")
+    if type(data_df) == type(None):
+      data_df = site_df
+    else:
+      data_df = pd.concat([data_df, site_df])
+          
+  return data_df
+
+def merge_site_metadata(data_df, site_metadata_df):
+  data_df = data_df.merge(site_metadata_df, how='left', left_on='site_id', right_on='site_id')
+  return data_df
+
+def check_and_drop_na(data_df):
+  if data_df.isna().sum().sum() > 0:
+    print("Data has NA.")
+    display(pd.DataFrame(data_df.isna().sum()).T)
+    data_df.dropna(axis=0, inplace=True)
+  else:
+    print("Datas has no NA.")
+    
