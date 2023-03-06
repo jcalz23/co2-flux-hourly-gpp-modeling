c29cec092f345478a70f3c8d5fbe60baf86a2e17
jcalz23 <john.calzaretta@berkeley.edu>
2023-03-06 03:42:25 +0000

    changing sagemaker conda setup

--- code/setup_conda.sh
+++ code/setup_conda.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -eux
 
-export REPOSITORY_URL="https://github.com/jcalz23/co2-flux-hourly-gpp-modeling.git"
-export REPOSITORY_NAME="co2-flux-hourly-gpp-modeling"
-
-if [ -d "$REPOSITORY_NAME" ]; then
-  echo "Directory 'git_directory' already exists"
-else
-  git clone $REPOSITORY_URL
-fi
-
-cd $REPOSITORY_NAME/code
 conda create --name py310 python=3.10.9 -y
 conda activate py310
 pip install --upgrade pip
