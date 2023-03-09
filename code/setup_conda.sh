4aa7898092a352d4de74fb4fdf1acceaa7ce555e
jcalz23 <john.calzaretta@berkeley.edu>
2023-03-09 21:50:50 +0000

    fixed setup_conda.sh

--- code/setup_conda.sh
+++ code/setup_conda.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+source /opt/conda/etc/profile.d/conda.sh
+
 conda create --name py310 python=3.10.9 -y
 conda activate py310
 pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt torch
+conda install ipykernel -y
