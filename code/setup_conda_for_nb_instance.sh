1f235e80a6bac74ec5b6e650517a3ee11ae356b7
EC2 Default User <ec2-user@ip-172-16-127-5.ec2.internal>
2023-04-07 22:51:14 +0000

    setup bash script for notebook instance

---
+++ code/setup_conda_for_nb_instance.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+# source /opt/conda/etc/profile.d/conda.sh
+conda create --name py310 python=3.10.9 -y
+conda activate py310
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt torch
+conda install ipykernel -y
