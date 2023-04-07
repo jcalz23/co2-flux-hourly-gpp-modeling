88a5bd4e158f0625bb4b66d576b73c45b7df297e
ruminakagawa <ruinakagawa@berkeley.edu>
2023-04-07 23:03:56 +0000

    update setup script for AWS notebook instance

--- code/setup_conda_for_nb_instance.sh
+++ code/setup_conda_for_nb_instance.sh
@@ -6,4 +6,5 @@ conda create --name py310 python=3.10.9 -y
 conda activate py310
 pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt torch
-conda install ipykernel -y
+# conda install ipykernel -y
+ipython kernel install --user --name=py310
