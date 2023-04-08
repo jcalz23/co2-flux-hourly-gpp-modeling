99b44a80c5325607789223b2c2554047b6ce98b6
ruminakagawa <ruminakagawa@berkeley.edu>
2023-04-08 23:41:44 +0000

    update setup script. Add notebooks for intepretability(ongoing)

--- code/setup_conda_for_nb_instance.sh
+++ code/setup_conda_for_nb_instance.sh
@@ -6,5 +6,9 @@ conda create --name py310 python=3.10.9 -y
 conda activate py310
 pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt torch
+pip install azure.storage.blob
+pip install --ignore-installed --upgrade tensorflow==2.5.0
+pip install pytorch_lightning
+pip install pytorch-forecasting==0.10.3
 # conda install ipykernel -y
 ipython kernel install --user --name=py310
